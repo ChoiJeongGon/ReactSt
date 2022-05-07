232f4aa14d8e0727b0d604103389d20c2e233d5c
Choi Jeong Gon <shinbaji@gmail.com>
2022-05-07 22:17:11 +0900

    Upload

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,17 +1,32 @@
-import logo from './logo.svg';
+// 터미널(WARNING 메세지 없애기) ->  /* eslint-disable */
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  let post = '우동 맛집';
-  
+  const logo = 'ReactBlog'
+  // State쓰던 html은 자동 재랜더링됨 (자주 변경)
+  let [글제목, b] = useState(['남자 코트 추천',' 우동 맛집', '파이썬독학']);
+  let [좋아요, 변경] = useState(0);
+ 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그</h4>
+        <h4>{logo}</h4>
       </div>
+
       <div className="list">
-        <h4>글 제목</h4>
+        <h4>{ 글제목[0] } <span onClick={ () => { 변경(좋아요+1) }}>👌</span> {좋아요} </h4>
+        <p>5월 7일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>5월 7일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
         <p>5월 7일 발행</p>
       </div>
     </div>
