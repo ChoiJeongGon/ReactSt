fadd9a047afe2c647474191b5c0e0d6f8ba19d3b
Choi Jeong Gon <shinbaji@gmail.com>
2022-05-07 22:33:53 +0900

    Upload

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,17 +6,25 @@ function App() {
 
   const logo = 'ReactBlog'
   // State쓰던 html은 자동 재랜더링됨 (자주 변경)
-  let [글제목, b] = useState(['남자 코트 추천',' 우동 맛집', '파이썬독학']);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천',' 우동 맛집', '파이썬독학']);
   let [좋아요, 변경] = useState(0);
  
+  function 제목바꾸기(){
+    // state를 deep copy 하기 [...]
+    const newArray = [...글제목];
+    newArray[0] = '여자 코트 추천';
+    글제목변경( newArray );
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
         <h4>{logo}</h4>
       </div>
-
+      <button onClick={제목바꾸기}>버튼</button>
       <div className="list">
-        <h4>{ 글제목[0] } <span onClick={ () => { 변경(좋아요+1) }}>👌</span> {좋아요} </h4>
+        <h4>{ 글제목[0] } <span onClick={ () => { 변경(좋아요+1) }}>👌</span> 
+        {좋아요} </h4>
         <p>5월 7일 발행</p>
       </div>
 
