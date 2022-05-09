c0662c63738203f3064c77bfd3c343338535b28c
Choi Jeong Gon <shinbaji@gmail.com>
2022-05-09 23:28:54 +0900

    basic Upload

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  const [text, setText] = useState('hello');
   const onSubmit = () => {
     alert("submitted");
   };
@@ -11,10 +12,24 @@ function App() {
     }
   };
 
+  // let text = 'hello1';
+
+  const updateText = () => {
+    // text = 'Coder';
+    setText('Coder')
+    console.log(text)
+  }
+
   return (
     <div className="App">
       <input onKeyUp={onKeyUp} />
       <button onClick={onSubmit}>Submit</button>
+
+
+      <br /><br />
+
+      <span>{text}</span>
+      <button onClick={updateText}>Update</button>
     </div>
   );
 }
