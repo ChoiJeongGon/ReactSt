7a594a1597b3a1464b55a19f56ce5bbc932fc790
Choi Jeong Gon <shinbaji@gmail.com>
2022-05-12 21:25:15 +0900

    Upload

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
-import Counter from "./components/Counter";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [buttonName, serButtonName] = useState('클릭');
-  const clickButton = () =>{
-    serButtonName('click');
-  }
-  return (
+  const [condition, setConditon] = useState(false);
+  const toggle = () => setConditon(!condition);
+  useEffect(() => {
+      console.log(condition);
+  },[condition])
+  const renderCondition = condition
+    ? 'True'
+    : 'False'
+  return(
     <div className="App">
-      <h1>Code</h1>
-      <Counter click="Click1"/>
-      <Counter click={buttonName}/>
-      <Counter />
-      <button onClick={clickButton}>Click</button>
+        <h1>Code</h1>
+        <div>
+       {renderCondition}
+        </div>
+        <button onClick={toggle}>Toggle</button>
     </div>
-  );
+  )
 }
 
 export default App;
