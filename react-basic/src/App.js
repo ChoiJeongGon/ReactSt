6cdec990d8584a24f84054477af8d785147b9c36
Choi Jeong Gon <shinbaji@gmail.com>
2022-05-12 21:19:29 +0900

    Upload

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -2,12 +2,17 @@ import React, { useState } from "react";
 import Counter from "./components/Counter";
 
 function App() {
+  const [buttonName, serButtonName] = useState('클릭');
+  const clickButton = () =>{
+    serButtonName('click');
+  }
   return (
     <div className="App">
       <h1>Code</h1>
+      <Counter click="Click1"/>
+      <Counter click={buttonName}/>
       <Counter />
-      <Counter />
-      <Counter />
+      <button onClick={clickButton}>Click</button>
     </div>
   );
 }
