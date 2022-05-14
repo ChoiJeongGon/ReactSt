3214abc9b06d6f194351659d43de980e38b5d141
Choi Jeong Gon <shinbaji@gmail.com>
2022-05-15 02:42:50 +0900

    Upload

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -5,6 +5,8 @@ function App() {
     { title: "coder1", year: 2001 },
     { title: "coder2", year: 2002 },
     { title: "coder3", year: 2003 },
+    { title: "coder4", year: 2004 },
+    { title: "coder5", year: 2005 },
   ];
 
   const renderMovies = movies.map(movie => {
