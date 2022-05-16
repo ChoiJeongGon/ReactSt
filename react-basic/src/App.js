3e9d85956c7eb792ad758e76ed427cbd0aa8358c
Choi Jeong Gon <shinbaji@gmail.com>
2022-05-16 20:36:06 +0900

    Upload

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,23 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Movie from "./components/Movie";
 
 function App() {
-  const movies = [
+  const [movieTitle, setMovieTitle] = useState("");
+  const [movieYear, setMovieYear] = useState("");
+  const [movies, setMovies] = useState([
     { title: "coder1", year: 2001 },
     { title: "coder2", year: 2002 },
     { title: "coder3", year: 2003 },
     { title: "coder4", year: 2004 },
-    { title: "coder5", year: 2005 },
-  ];
+  ]);
+  useEffect(() =>{
+      console.log('render');
+  });
 
-  const renderMovies = movies.map(movie => {
+  const renderMovies = movies.map((movie) => {
     return (
-      <Movie movie={movie} key={movie.title}/>
+    <Movie movie={movie} key={movie.title} />
     );
   });
+  const addMovie = (event) =>{
+    event.preventDefault();
+    setMovies([
+      ...movies,
+      {
+      title: movieTitle,
+      year: movieYear,
+    }])
+  }
   return (
     <div className="App">
       <h1>Moive List</h1>
+      <form onSubmit={addMovie}>
+        <input
+          type="text"
+          value={movieTitle}
+          placeholder="영화제목"
+          onChange={e => setMovieTitle(e.target.value)}
+        ></input>
+        <br />
+        <input
+          type="text"
+          value={movieYear}
+          placeholder="개봉년도"
+          onChange={e => setMovieYear(e.target.value)}
+        ></input>
+        <br />
+        <button type="submit">영화추가</button>
+      </form>
       {renderMovies}
     </div>
   );
